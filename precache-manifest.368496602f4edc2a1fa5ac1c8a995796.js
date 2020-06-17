f0c0bbde2ef0e07d1b4a141e6445c1ed61a37ea4
Prashanth Reddy <prashanth reddy>
2020-06-17 21:29:33 +0530

    Updates

---
+++ precache-manifest.368496602f4edc2a1fa5ac1c8a995796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5335731ddb6caa139ed6ab6b87f130",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "4acea1106b5ec16e08c9",
+    "url": "/react-todo-app/static/css/main.9a4115ce.chunk.css"
+  },
+  {
+    "revision": "faef0401ff180e578be4",
+    "url": "/react-todo-app/static/js/2.ee62a020.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.ee62a020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4acea1106b5ec16e08c9",
+    "url": "/react-todo-app/static/js/main.eb2c94ed.chunk.js"
+  },
+  {
+    "revision": "a02306a5582400e4c981",
+    "url": "/react-todo-app/static/js/runtime-main.15b7a15e.js"
+  }
+]);
